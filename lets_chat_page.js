96e7e026027b71200b9eb2eb200e364b9be68327
vihaan1245 <96100376+vihaan1245@users.noreply.github.com>
2022-01-03 07:47:15 +0100

    Add files via upload

---
+++ lets_chat_page.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAoh2DsG_ImLF-Hs7j2H-vD_npjFq5nn6I",
+    authDomain: "c-93-project-17876.firebaseapp.com",
+    databaseURL: "https://c-93-project-17876-default-rtdb.firebaseio.com",
+    projectId: "c-93-project-17876",
+    storageBucket: "c-93-project-17876.appspot.com",
+    messagingSenderId: "1008244353984",
+    appId: "1:1008244353984:web:b98424d31f8f6976af7625"
+  };
+  
+  const app = initializeApp(firebaseConfig);
